c7dfede54ba9dd6ec1eca1135990cf770b24784b
Heiner Morales <hmorales@outcoding.com>
2022-06-06 15:47:53 -0600

    Cambiando Fruta

--- PracticaPOO/Clases/Fruta.cs
+++ PracticaPOO/Clases/Fruta.cs
@@ -1,18 +1,10 @@
-﻿using PracticaPOO.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static PracticaPOO.Program;
-
-namespace PracticaPOO
+﻿namespace PracticaPOO
 {
     public class Fruta : ProdVegetal
     {
 		public Enums.Fruta Tipo { get; set; }
 
-		public Fruta() : base("Fruta", 100, 1) { }
+		public Fruta() : base("Fruta", 200, 1) { }
 
 		public Fruta (Enums.Fruta tipo) : this()
 		{
