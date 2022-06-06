3b3aa35cfba610749f74300a2ba570e091f9b5e6
Roberto Lezama <rlezama@outcoding.com>
2022-06-06 14:59:53 -0600

    Cambié el documento

--- PracticaPOO/Clases/Fruta.cs
+++ PracticaPOO/Clases/Fruta.cs
@@ -12,7 +12,7 @@ namespace PracticaPOO
     {
 		public Enums.Fruta Tipo { get; set; }
 
-		public Fruta() : base("Fruta", 100, 1) { }
+		public Fruta() : base("Fruta", 400, 1) { }
 
 		public Fruta (Enums.Fruta tipo) : this()
 		{
